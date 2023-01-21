d57c5086bc791590b15db6e931ee15c6ae42b93a
jenil12345678990 <jenil@seveninfosystem.com>
2023-01-21 15:16:44 +0530

    Added the New Project
    sadjkfjksdnf

---
+++ GitHubTesting/GitHubTesting/GitHubTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestingApp.swift
+//  GitHubTesting
+//
+//  Created by SevenInfosystem on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
